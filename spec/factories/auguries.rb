94fd588680b0b3e4c3c6fab58c679d45aa1b9d51
TAKUYA34 <110417799+TAKUYA34@users.noreply.github.com>
2022-12-22 21:54:14 +0900

    テストコード(投稿機能)

--- spec/factories/auguries.rb
+++ spec/factories/auguries.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :augury do
-    
+    nickname                  {Faker::Name.initials}
+    sign                      {"魚座"}
+    birth_id                  {'1933-2-4'}
+    association :user
   end
 end
--- spec/models/augury_spec.rb
+++ spec/models/augury_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Augury, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @augury = FactoryBot.build(:augury)
+  end
+
+  describe '占い投稿機能' do
+    context '占いが投稿できる' do
+      it '必要な情報が全て存在すれば登録できる' do
+        expect(@augury).to be_valid
+      end
+      it 'nicknameが空では登録できない' do
+        @augury.nickname = ''
+        @augury.valid?
+        expect(@augury.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it 'signが空では登録できない' do
+        @augury.sign = ''
+        @augury.valid?
+        expect(@augury.errors.full_messages).to include("Sign can't be blank")
+      end
+      it 'birth_idが空では登録できない' do
+        @augury.birth_id = ''
+        @augury.valid?
+        expect(@augury.errors.full_messages).to include("Birth can't be blank")
+      end
+    end
+  end
 end
+
+
